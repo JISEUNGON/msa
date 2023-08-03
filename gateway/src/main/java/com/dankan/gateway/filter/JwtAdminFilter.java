435dbf2dbffd0ccc79345b43a21905d1085708f2
Qkdrmf <djs110615@mju.ac.kr>
2023-08-03 22:01:23 +0900

    Feat: filter 적용

---
+++ gateway/src/main/java/com/dankan/gateway/filter/JwtAdminFilter.java
@@ -0,0 +1,89 @@
+package com.dankan.gateway.filter;
+
+import com.dankan.gateway.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+public class JwtAdminFilter extends AbstractGatewayFilterFactory<JwtAdminFilter.Config> {
+    public JwtAdminFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(final Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            //header 값
+            String authorizationHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            // bearer이 아니면 오류
+            if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                // 응답 내용 수정
+                String modifiedResponse = "JWT Token does not begin with Bearer String.";
+                DataBuffer newResponseData = response.bufferFactory().wrap(modifiedResponse.getBytes(StandardCharsets.UTF_8));
+                response.getHeaders().setContentLength(modifiedResponse.length());
+
+                return response.writeWith(Flux.just(newResponseData));
+            }
+
+            // Token 꺼내기
+            String token = authorizationHeader.split(" ")[1];
+
+            // Token 검증
+            if (!JwtUtil.validateToken(token)) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                // 응답 내용 수정
+                String modifiedResponse = "JWT Token is not valid.";
+                DataBuffer newResponseData = response.bufferFactory().wrap(modifiedResponse.getBytes(StandardCharsets.UTF_8));
+                response.getHeaders().setContentLength(modifiedResponse.length());
+
+                return response.writeWith(Flux.just(newResponseData));
+            }
+
+            // Token 만료 체크
+            if (JwtUtil.isExpired(token)) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                // 응답 내용 수정
+                String modifiedResponse = "JWT Token is expired.";
+                DataBuffer newResponseData = response.bufferFactory().wrap(modifiedResponse.getBytes(StandardCharsets.UTF_8));
+                response.getHeaders().setContentLength(modifiedResponse.length());
+
+                return response.writeWith(Flux.just(newResponseData));
+            }
+
+            if(JwtUtil.getRoles(token).contains("ROLE_ADMIN")) {
+                log.info("role: {}", "ROLE_ADMIN");
+            }
+            else {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                // 응답 내용 수정
+                String modifiedResponse = "User does not have permission.";
+                DataBuffer newResponseData = response.bufferFactory().wrap(modifiedResponse.getBytes(StandardCharsets.UTF_8));
+                response.getHeaders().setContentLength(modifiedResponse.length());
+
+                return response.writeWith(Flux.just(newResponseData));
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+
+    }
+}
--- gateway/src/main/java/com/dankan/gateway/util/JwtUtil.java
+++ gateway/src/main/java/com/dankan/gateway/util/JwtUtil.java
@@ -1,82 +1,30 @@
 package com.dankan.gateway.util;
 
-import com.dankan.domain.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Component
 public class JwtUtil {
     public static String JWT_SECRET_KEY;
-    private static final long EXPIRATION_TIME =  1000 * 60 * 60 * 24 * 365; // 365일
-    private static final long REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 365; // 365일
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 365;
 
     @Value("${jwt.secret}")
     public void setKey(String key) {
         JWT_SECRET_KEY = key;
     }
 
-    public static String createJwt(User user) {
-        Date now = new Date();
-
-        Date expiredDate = new Date(now.getTime() + EXPIRATION_TIME);
-
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("id", user.getUserId());
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(expiredDate)
-                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
-                .compact();
-    }
-
-    public static String logout() {
-        Date now = new Date();
-
-        Date expiredDate = new Date(now.getTime() - EXPIRATION_TIME);
-
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("id", JwtUtil.getMemberId());
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(expiredDate)
-                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
-                .compact();
-    }
-
-    // jwt refresh 토큰 생성
-    public static String createRefreshToken() {
-        Date now = new Date();
-        return Jwts.builder()
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + REFRESH_TOKEN_EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
-                .compact();
-    }
-
     public static boolean isExpired(String token) {
-        boolean isExpired = true;
+        boolean isExpired;
 
         try {
-            Date expiredDate = Jwts.parserBuilder()
+            Jwts.parserBuilder()
                     .setSigningKey(JWT_SECRET_KEY)
                     .build()
                     .parseClaimsJws(token)
@@ -93,16 +41,14 @@ public class JwtUtil {
         return isExpired;
     }
 
-    public static Long getMemberId() {
-        String token = JwtUtil.getAccessToken();
-
+    public static List<String> getRoles(String token) {
         Claims body = Jwts.parserBuilder()
                 .setSigningKey(JWT_SECRET_KEY)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
 
-        return body.get("id", Long.class);
+        return body.get("roles", List.class);
     }
 
     /**
@@ -118,35 +64,5 @@ public class JwtUtil {
         } catch (Exception e) {
             return false;
         }
-    }
-
-    public static String getAccessToken() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        String token = null;
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            token = authorizationHeader.substring(7);
-        }
-        else
-        {
-            throw new RuntimeException();
-        }
-
-        return token;
-    }
-
-    public static String getRefreshToken() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-        String authorizationHeader = request.getHeader("RefreshToken");
-        String token = null;
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            token = authorizationHeader.substring(7);
-        }
-        else
-        {
-            throw new RuntimeException();
-        }
-
-        return token;
     }
 }
